139230c32f4ea7bbf16abc9f006d1e80bc8cbc5f
baiyanwei <byw-martin@163.com>
2016-04-29 16:04:34 +0800

    import bootstrap css framework

---
+++ src/main/webapp/demo/offcanvas.js
@@ -0,0 +1,8 @@
+/**
+ * Created by martin on 4/29/16.
+ */
+$(document).ready(function () {
+    $('[data-toggle="offcanvas"]').click(function () {
+        $('.row-offcanvas').toggleClass('active')
+    });
+});
